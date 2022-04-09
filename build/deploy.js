0efa621f67ec9203186d7072a14fa0834564f548
borecjeborec1 <robingrundel@seznam.cz>
2022-04-09 13:48:03 +0200

    license

--- build/deploy.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = require("fs");
-var child_process_1 = require("child_process");
-(0, child_process_1.spawnSync)("cd normal && cargo build --release");
-(0, child_process_1.spawnSync)("cd weak && cargo build --release");
-(0, fs_1.copyFileSync)(__dirname + "/../normal/target/release/league-stop.exe", __dirname + "/../deploy/league-stop.exe");
-(0, fs_1.copyFileSync)(__dirname + "/../weak/target/release/league-stop-weak.exe", __dirname + "/../deploy/league-stop-weak.exe");
